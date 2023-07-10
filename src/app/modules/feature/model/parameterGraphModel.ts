5ad139be090ee9c0085561789d6c65ad6cbe5ac4
Gayatri Kadam <gayatrik7@birlasoft.com>
2023-07-10 21:54:16 +0530

    Parameter chart changes

---
+++ src/app/modules/feature/model/parameterGraphModel.ts
@@ -0,0 +1,13 @@
+export class ParameterGraphModel{
+    
+    date!:string ;
+    value!:number;
+    well!:string;
+
+    constructor()
+    {        
+        this.date="";
+        this.value=0;
+        this.well="";
+    }
+}
