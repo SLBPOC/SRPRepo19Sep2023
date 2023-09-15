4425873e82681f4cfb7f15e4777ecdc28e02ee09
Shivangi Soni <shivangis5@birlasoft.com>
2023-09-15 15:56:01 +0530

    Added charts for alerts page

--- src/app/modules/feature/components/alert-categories-chart/alert-categories-chart.component.ts
+++ src/app/modules/feature/components/alert-categories-chart/alert-categories-chart.component.ts
@@ -1,74 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import * as Highcharts from 'highcharts';
 @Component({
   selector: 'app-alert-categories-chart',
   templateUrl: './alert-categories-chart.component.html',
   styleUrls: ['./alert-categories-chart.component.scss']
 })
-export class AlertCategoriesChartComponent {
-  Highcharts: typeof Highcharts = Highcharts;
-  chartOptions: Highcharts.Options = {
-    chart: {
-      type: 'pie',
-      
-    },
-    title: {
-      text: ''
-    },
-    plotOptions: {
-      pie: {
-        innerSize: '50%',
-         dataLabels: {
-          enabled: false
-        },
-        showInLegend: true,
-      }
-    },
-    legend: {
-      align: 'left', 
-      layout: 'horizontal', 
-      verticalAlign: 'top', 
-      labelFormat: '<b>{name}</b>: {percentage:.1f}%',
-      symbolRadius: 0,
-    },
-    series: [
-      
-    ]
-  };
-  constructor(){
-    this.chartOptions.series =  [
-      {
-        type: 'pie', 
-        name: 'Cycle Status',
-        data: [
-          {
-            name: 'Gas Interference Events',
-            y: 15,
-            color: '#F38888'
-          },
-          {
-            name: 'Fluid Pound Events',
-            y: 25,
-            color: '#8760CC'
-          },
-          {
-            name: 'Flatlining Events',
-            y: 25,
-            color: '#7DCCD8'
-          },
-          {
-            name: 'Tagging Events',
-            y: 15,
-            color: '#3097A7'
-          },
-          {
-            name: 'Current SPM',
-            y: 10,
-            color: '#DA44AA'
-          }
+export class AlertCategoriesChartComponent implements OnInit, OnChanges {
+  @Input() chartData: any;
 
-        ]
+  Highcharts: typeof Highcharts = Highcharts;
+  chartOptions: Highcharts.Options;
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+  ngOnChanges() {
+    this.loadChartData();
+  }
+
+  loadChartData() {
+    let chartSeriesArr = []
+    let obj = {}
+    for (let i = 0; i < this.chartData.length; i++) {
+      obj = {
+        name: this.chartData[i].name,
+        y: this.chartData[i].value
       }
-    ]
+      chartSeriesArr.push(obj)
+    }
+
+    this.chartOptions = {
+      chart: {
+        type: 'pie',
+
+      },
+      title: {
+        text: ''
+      },
+      plotOptions: {
+        pie: {
+          innerSize: '50%',
+          dataLabels: {
+            enabled: false
+          },
+          showInLegend: true,
+        }
+      },
+      legend: {
+        align: 'left',
+        layout: 'horizontal',
+        verticalAlign: 'top',
+        labelFormat: '<b>{name}</b>: {percentage:.1f}%',
+        symbolRadius: 0,
+      },
+      series: [
+        {
+          type: 'pie',
+          name: 'Cycle Status',
+          data: chartSeriesArr
+        }
+      ]
+    };
   }
 }
