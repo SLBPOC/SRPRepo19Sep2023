0072464fa9adccca5e592a29999aa98a97eb19ae
Subhash P <subhashp1@birlasoft.com>
2023-09-26 14:48:17 +0530

    default field checkbox enable

--- src/app/modules/feature/components/well-tree-list/well-tree-search/well-tree-search.component.ts
+++ src/app/modules/feature/components/well-tree-list/well-tree-search/well-tree-search.component.ts
@@ -1,16 +1,20 @@
 import { Component, Output, EventEmitter, Inject } from '@angular/core';
 import { TreeViewService } from '../../../services/tree-view.service';
-import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
+import {
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogRef,
+} from '@angular/material/dialog';
 import { MatSelectChange } from '@angular/material/select';
 import { SavedState, Node, NodeType } from '../../../services/models';
 
 @Component({
   selector: 'app-well-tree-search',
   templateUrl: './well-tree-search.component.html',
-  styleUrls: ['./well-tree-search.component.css']
+  styleUrls: ['./well-tree-search.component.css'],
 })
 export class WellTreeSearchComponent {
-  option: string = "fields"
+  option: string = 'fields';
   searchText: string;
   isSaveEnabled: boolean = false;
   hasSavedList: boolean = false;
@@ -18,47 +22,60 @@ export class WellTreeSearchComponent {
   currentSelectedSavedSearch: SavedState | null;
   selectedNodes: Node[];
   selectedNodeTypes: NodeType[] = [];
-  fieldCheckbox: boolean = true;
+  fieldCheckbox: boolean = false;
   batteryCheckbox: boolean = false;
   padCheckbox: boolean = false;
   wellCheckbox: boolean = false;
 
   @Output()
-  userChanged: EventEmitter<any> = new EventEmitter<any>()
+  userChanged: EventEmitter<any> = new EventEmitter<any>();
 
-  constructor(public treeViewService: TreeViewService, public dialog: MatDialog) {
-    this.treeViewService.selectedNodes.subscribe(x => {
-      this.isSaveEnabled = x != null && x != undefined && x.length != 0 && this.currentSelectedSavedSearch == null;
-    })
-    this.treeViewService.savedStateEmit.subscribe(x => this.hasSavedList = x.length > 0);
-    this.treeViewService.selectedSavedTreeStateEvent.subscribe(x => {
+  constructor(
+    public treeViewService: TreeViewService,
+    public dialog: MatDialog
+  ) {
+    this.treeViewService.selectedNodes.subscribe((x) => {
+      this.isSaveEnabled =
+        x != null &&
+        x != undefined &&
+        x.length != 0 &&
+        this.currentSelectedSavedSearch == null;
+    });
+    this.treeViewService.savedStateEmit.subscribe(
+      (x) => (this.hasSavedList = x.length > 0)
+    );
+    this.treeViewService.selectedSavedTreeStateEvent.subscribe((x) => {
       this.currentSelectedSavedSearch = x.state;
       if (x.fresh) {
         this.option = <string>x.state?.SavedOption || 'fields';
         this.searchText = <string>x.state?.SavedText || '';
-        this.onSearchChange()
-      }
-      else if (x.state == null) {
+        this.onSearchChange();
+      } else if (x.state == null) {
         this.option = 'fields';
         this.searchText = '';
-        this.onSearchChange()
+        this.onSearchChange();
       }
     });
-    this.treeViewService.selectedNodes.subscribe(x => {
+    this.treeViewService.selectedNodes.subscribe((x) => {
       this.selectedNodes = x;
     });
     this.selectedNodeTypes.push(NodeType.Field);
   }
 
   onCheckBoxChange(event: any, nodeType: NodeType) {
-    if (event.checked)
-      this.selectedNodeTypes.push(nodeType);
+    if (event.checked) this.selectedNodeTypes.push(nodeType);
     else
-      this.selectedNodeTypes.splice(this.selectedNodeTypes.findIndex(x => x == nodeType), 1);
+      this.selectedNodeTypes.splice(
+        this.selectedNodeTypes.findIndex((x) => x == nodeType),
+        1
+      );
     this.onSearchChange();
   }
   onSearchChange() {
-    this.userChanged.emit({ option: this.selectedNodeTypes, searchText: this.searchText });
+    this.userChanged.emit({
+      option: this.selectedNodeTypes,
+      searchText: this.searchText,
+    });
   }
   onClear() {
     this.treeViewService.setSelectedSavedTreeState(null);
@@ -76,54 +93,50 @@ export class WellTreeSearchComponent {
       Name: this.savedStateName,
       SavedOption: this.option,
       SavedText: this.searchText,
-      SelectedNode: this.selectedNodes
+      SelectedNode: this.selectedNodes,
     });
     this.isSaveEnabled = false;
   }
   openSaveDialog(): void {
     const dialogRef = this.dialog.open(SaveTreeStateDialog, {
       width: '250px',
-      data: this.savedStateName
+      data: this.savedStateName,
     });
 
-    dialogRef.afterClosed().subscribe(result => {
+    dialogRef.afterClosed().subscribe((result) => {
       if (result != undefined && result != null && result.trim() != '') {
         this.savedStateName = result;
         this.save();
-        this.savedStateName = "";
+        this.savedStateName = '';
         this.isSaveEnabled = false;
       }
     });
   }
   onSelect(event: MatSelectChange) {
     console.log(event);
-    this.treeViewService.setSelectedSavedTreeState(event.value, true)
+    this.treeViewService.setSelectedSavedTreeState(event.value, true);
   }
 }
 
-
 @Component({
   selector: 'save-tree-state-dialog',
-  template: `
-  <div mat-dialog-content>
-    <mat-form-field>
-      <input placeholder="Enter a name" matInput [(ngModel)]="data">
-    </mat-form-field>
-  </div>
-  <div mat-dialog-actions>
-    <button mat-button (click)="onNoClick()">Cancel</button>
-    <button mat-button [mat-dialog-close]="data" cdkFocusInitial>Save</button>
-  </div>`,
+  template: ` <div mat-dialog-content>
+      <mat-form-field>
+        <input placeholder="Enter a name" matInput [(ngModel)]="data" />
+      </mat-form-field>
+    </div>
+    <div mat-dialog-actions>
+      <button mat-button (click)="onNoClick()">Cancel</button>
+      <button mat-button [mat-dialog-close]="data" cdkFocusInitial>Save</button>
+    </div>`,
 })
 export class SaveTreeStateDialog {
-
   constructor(
     public dialogRef: MatDialogRef<SaveTreeStateDialog>,
-    @Inject(MAT_DIALOG_DATA) public data: string) { }
+    @Inject(MAT_DIALOG_DATA) public data: string
+  ) {}
 
   onNoClick(): void {
     this.dialogRef.close();
   }
-
 }
-
